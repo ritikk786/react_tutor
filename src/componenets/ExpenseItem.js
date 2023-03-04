2145085774a32f5f4e8fbefd671752dc080ad757
ritikk786 <kumarritik9540@gmail.com>
2023-03-04 20:24:15 +0530

    task7

---
+++ src/componenets/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+        <div >
+            <h4>Expense Item</h4>
+            <ul>
+                <li>Food Rs 10</li>
+                <li>Petrol Rs 100</li>
+                <li>Movies Rs 200</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ExpenseItem
